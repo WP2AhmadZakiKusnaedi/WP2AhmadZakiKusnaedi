97cd4610de86c8a25befa584fc738fd5cbbca1ff
ahmadzaki <90685339+WP2AhmadZakiKusnaedi@users.noreply.github.com>
2021-09-27 20:06:30 +0700

    Add files via upload

---
+++ v_home.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Hello~</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+
+	<style type="text/css">
+		body {
+			background: linear-gradient(#3347FF, #33FF35);
+			height: 70vh;
+		}
+
+		.home {
+			display: flex;
+			align-items: center;
+			justify-content: center;
+			margin-top: 230px;
+		}
+	</style>
+</head>
+
+<body>
+	<div class="home">
+		<div class="alert alert-secondary" role="alert">
+			<h4 class="alert-heading">Selamat Datang Ahmad Zaki di Penjumlahan Codeigniter</h4>
+			<p>Untuk melakukan penjumlahan kita menggunakan url <br>
+				url penjumlahan kita share di bawah<br></p>
+			<hr>
+			<form>
+				<p class="mb-0">http://localhost/217/latihan/penjumlahan/<span>227</span>/<span>377</span></p>
+			</form>
+		</div>
+	</div>
+
+</body>
+
+</html>
